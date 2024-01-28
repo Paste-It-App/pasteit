0c00ba825a81400fb7851bfa18820abfab340161
Iyad Ullah Zuberi <iyadz.personal@gmail.com>
2024-01-28 03:47:20 +0000

    Created sign up front end form

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import SignUpForm from "./signUpForm.svelte";
+
+</script>
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        color: #FFFFFF;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+
+<SignUpForm/>
+
